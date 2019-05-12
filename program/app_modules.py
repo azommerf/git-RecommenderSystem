1d1adb6a82741d290c049b55b6681e96147c9ff5
azommerf <andre.zommerfelds@unil.ch>
2019-05-13 01:05:34 +0200

    latest

--- program/app_modules.py
+++ program/app_modules.py
@@ -106,13 +106,17 @@ def data_download(url):
                 print("\nData file not found. Download the file to continue program.")
 
     if os.path.isfile(filepath_tsv):
-        msg = "Data downloaded successfully (check data directory)"
+        msg = "Data downloaded successfully (check data directory)."
         return msg
     else:
-        msg = "Something went wrong. Please try again or send a message to on of our hosts: andre.zommerfelds@unil.ch"
+        msg = "Something went wrong. Please try again or send a message to on of our hosts: andre.zommerfelds@unil.ch."
         return msg
                 
 def data_frame(url):
+
+    #################################################################################
+    # Create dataframe
+
     try:
         filename = url.split('/')[-1]
         workingdir = os.path.abspath('')
@@ -125,9 +129,63 @@ def data_frame(url):
         print("\n...creating dataframe...")
         df = pd.read_csv(filepath_tsv, delimiter='\t', encoding="utf-8", error_bad_lines=False)
 
-        msg = "Dataframe created successfully."
+        msg = "Dataframe created successfully"
 
     except:
         msg = "Encountered an error while creating dataframe."
 
     return df, msg
+
+
+def data_KNN(df):
+    #################################################################################
+    # Nearest neighbors method
+    # (1) Create sparse matrix with unique customer and unique item index and the corresponding star rating
+    #    needed data: start_rating, product_id, customer_id
+    # (2) Compute nearest neighbors
+
+    try:
+
+        # Calculate total number of products and customers (for range calculation)
+        prodNo = len(set(df["product_id"])) # Number of products
+        custNo = len(set(df["customer_id"])) # Number of customers
+        print("Number of unique products: {}".format(prodNo))
+        print("Number of unique customers: {}".format(custNo))
+
+        # Get unique product and customer IDs and index them
+        prodUnique_indexed = dict(zip(np.unique(df["product_id"]), list(range(prodNo))))
+        custUnique_indexed = dict(zip(np.unique(df["customer_id"]), list(range(custNo))))
+        prodUnique_reverseIndexed = dict(zip(list(range(prodNo)), np.unique(df["product_id"])))
+        custUnique_reverseIndexed = dict(zip(list(range(custNo)), np.unique(df["customer_id"])))
+
+        # Finally go through each row of dataframe and assign index 
+        custIndex = [custUnique_indexed[i] for i in df["customer_id"]]
+        prodIndex = [prodUnique_indexed[i] for i in df["product_id"]]
+
+        # Create indexed matrix
+        df_custIndex = pd.DataFrame(custIndex, columns=["custIndex"])
+        df_prodIndex = pd.DataFrame(prodIndex, columns=["prodIndex"])
+        df_indexed = pd.DataFrame(df["star_rating"], columns=["star_rating"])
+        df_indexed.insert(0, "prodIndex", df_prodIndex)
+        df_indexed.insert(0, "custIndex", df_custIndex)
+
+
+        # Create the sparse matrix based on a matrix with dimensions 
+        # (no. of unique products x no. of unique customres)
+        # Description from official scipy documentation:
+        # csr_matrix((data, (row_ind, col_ind)), [shape=(M, N)])
+        # where data, row_ind and col_ind satisfy the relationship a[row_ind[k], col_ind[k]] = data[k].
+
+        df_csr = csr_matrix((df_indexed["star_rating"], (prodIndex, custIndex)), shape=(prodNo, custNo))
+
+        msg = "\nFitted the KNN model successfully"
+
+        return prodNo, custNo,\
+            prodUnique_indexed, custUnique_indexed,\
+            prodUnique_reverseIndexed, custUnique_reverseIndexed,\
+            custIndex, prodIndex,\
+            df_custIndex, df_prodIndex, df_indexed, df_csr, msg
+
+    except: 
+        msg = "\nError during KNN model fitting encountered"
+        print(msg)
--- program/app_server.py
+++ program/app_server.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, render_template, url_for
+import os
+from flask import Flask, request, render_template, url_for, session
 import subprocess
-from program.app_modules import data_download, data_frame
+from program.app_modules import data_download, data_frame, data_KNN
 import random, threading, webbrowser
 import pandas as pd
 
@@ -12,6 +13,9 @@ def main():
     home_url = "http://127.0.0.1:{0}".format(port)
     threading.Timer(1.25, lambda: webbrowser.open(home_url) ).start()
 
+    def return_function(parameter):
+        return parameter
+
     @app.route("/")
     def home():
         return render_template('home.html')
@@ -20,25 +24,38 @@ def main():
     def cockpit():
         return render_template('cockpit.html')
 
-
     @app.route("/cockpit/download", methods=['POST'])
     def choose_dataset():
         _data_url = request.form["_data_url"] # Save dataset
         print("\nDownload process started for the following file: "+_data_url)
         _success_msg = data_download(_data_url)
         print(_success_msg)
-        # return dataset(_success_msg)
         return render_template('dataset.html', msg=_success_msg)
 
     @app.route("/cockpit/dataframe", methods=['POST'])
     def create_dataframe():
         _data_url = request.form["_data_url"] # Save dataset
-        print("\nDownload process started for the following file: "+_data_url[0:-3])
+        print("\nCreating dataframe from the following file: "+_data_url.split('/')[-4])
         df, msg = data_frame(_data_url)
-        df.head()
-        return render_template('dataframe.html', msg=msg)
+        # session['df'] = df
+        print(df.head())
+        print("Saving dataframe as csv.\Printing dataframe to HTML.")
+        df.to_csv("program/data/df.csv")
+        print("CSV file created.")
+        return render_template('dataframe.html', msg=msg, data=df.head().to_html())
 
-
+    @app.route("/cockpit/KNN", methods=['POST'])
+    def fit_KNN():
+        print("\nReading the dataframe as csv.")
+        df = pd.read_csv("program/data/df.csv")
+        print(df.head())
+        # prodNo, custNo,\
+        # prodUnique_indexed, custUnique_indexed,\
+        # prodUnique_reverseIndexed, custUnique_reverseIndexed,\
+        # custIndex, prodIndex,\
+        # df_custIndex, df_prodIndex, df_indexed, df_csr, msg = data_KNN(df)
+        msg = "KNN model fitted?"
+        return render_template('KNN.html', msg=msg)
 
 
     app.run(port=port, debug=False)
