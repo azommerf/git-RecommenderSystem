e5c255e6512e92c301d206c9b32c0e926b0669bb
azommerf <andre.zommerfelds@unil.ch>
2019-05-15 23:31:39 +0200

    latest

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 import numpy as np
 
-
+# url = "https://s3.amazonaws.com/amazon-reviews-pds/tsv/amazon_reviews_us_Wireless_v1_00.tsv.gz"
 url = "https://s3.amazonaws.com/amazon-reviews-pds/tsv/amazon_reviews_us_Books_v1_00.tsv.gz"
 # url = "https://s3.amazonaws.com/amazon-reviews-pds/tsv/amazon_reviews_us_Musical_Instruments_v1_00.tsv.gz"
 
@@ -15,21 +15,36 @@ filepath_tsv = filepath_tsvgz[0:-3]
 
 print("creating dataframe")
 
-usecols = ['customer_id','product_id','star_rating',]
-# dtype = {'star_rating':np.int32}
+
+def conv(val):
+    if not val:
+        return 0    
+    try:
+        return np.int8(val)
+    except:        
+        return np.int8(0)
+
+usecols = ['customer_id','product_id','star_rating']
+dtype = {'customer_id':str, 'product_id':str, 'star_rating':int}
+
 df = pd.read_csv(filepath_tsvgz,\
                 delimiter='\t',\
                 encoding="utf-8",\
                 error_bad_lines=False,\
                 compression='gzip',\
-                # dtype=dtype,\
-                usecols=usecols)
+                #dtype=dtype,\
+                usecols=usecols,\
+                #low_memory=False,\
+                converters={'star_rating':conv})
 # Clean dataframe
-df = df[df['star_rating'].isin([0,1,2,3,4,5])]
-df.astype({'star_rating':np.int8})
+# df = df[df['star_rating'].isin([0,1,2,3,4,5])]
+# df.astype({'star_rating':np.int8})
 
 print("created successfully")
 print(df.head())
+
+from scipy.sparse import csr_matrix, save_npz, load_npz # Sparse matrix
+from sklearn.neighbors import NearestNeighbors
 
 # Calculate total number of products and customers (for range calculation)
 print("success 1")
@@ -44,11 +59,13 @@ prodUnique_indexed = dict(zip(np.unique(df["product_id"]), list(range(prodNo))))
 custUnique_indexed = dict(zip(np.unique(df["customer_id"]), list(range(custNo))))
 prodUnique_reverseIndexed = dict(zip(list(range(prodNo)), np.unique(df["product_id"])))
 custUnique_reverseIndexed = dict(zip(list(range(custNo)), np.unique(df["customer_id"])))
+print("success 2.1")
 
 # Finally go through each row of dataframe and assign index 
 print("success 3")
 custIndex = [custUnique_indexed[i] for i in df["customer_id"]]
 prodIndex = [prodUnique_indexed[i] for i in df["product_id"]]
+print("success 3.1")
 
 # Create indexed matrix
 print("success 4")
@@ -57,7 +74,7 @@ df_prodIndex = pd.DataFrame(prodIndex, columns=["prodIndex"])
 df_indexed = pd.DataFrame(df["star_rating"], columns=["star_rating"])
 df_indexed.insert(0, "prodIndex", df_prodIndex)
 df_indexed.insert(0, "custIndex", df_custIndex)
-
+print("success 4.1")
 
 # Create the sparse matrix based on a matrix with dimensions:
 # (no. of unique products x no. of unique customres).
@@ -68,15 +85,5 @@ df_indexed.insert(0, "custIndex", df_custIndex)
 print("...creating sparse matrix...")
 df_csr = csr_matrix((df_indexed["star_rating"], (prodIndex, custIndex)), shape=(prodNo, custNo))
 
-print("...storing number of customers...")
-with open("./program/data/custNo.pickle", "wb") as pkl:
-    pickle.dump(custNo, pkl, protocol=pickle.HIGHEST_PROTOCOL)
-print("...storing indices...")
-with open("./program/data/prodUnique_indexed.pickle", "wb") as pkl:
-    pickle.dump(prodUnique_indexed, pkl, protocol=pickle.HIGHEST_PROTOCOL)
-with open("./program/data/prodUnique_reverseIndexed.pickle", "wb") as pkl:
-    pickle.dump(prodUnique_reverseIndexed, pkl, protocol=pickle.HIGHEST_PROTOCOL)
-print("...storing sparse matrix...")
-save_npz("./program/data/df_csr.npz", df_csr)
-
 msg = "\nFitted the KNN model successfully."
+print(msg)
